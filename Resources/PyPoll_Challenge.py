a909379b6b809a34b360d7e5eac9497deac01dbb
Emily Costello <eccostello1@gmail.com>
2022-09-10 16:17:38 -0400

    Adding election analysis.

--- Resources/PyPoll_Challenge.py
+++ Resources/PyPoll_Challenge.py
@@ -6,7 +6,7 @@ import csv
 import os
 
 # Add a variable to load a file from a path.
-file_to_load = os.path.join("..", "Resources", "election_results.csv")
+file_to_load = os.path.join("Resources", "election_results.csv")
 # Add a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
@@ -18,7 +18,7 @@ candidate_options = []
 candidate_votes = {}
 
 # 1: Create a county list and county votes dictionary.
-county_list = []
+county_options = []
 county_votes = {}
 
 
@@ -31,8 +31,8 @@ winning_percentage = 0
 # 2: Track the largest county and county voter turnout.
 
 winning_county = ""
-winning_countyvotes = 0
-
+winning_countycount = 0
+winning_countypercentage = 0
 
 # Read the csv and convert it into a list of dictionaries
 with open(file_to_load) as election_data:
@@ -68,10 +68,10 @@ with open(file_to_load) as election_data:
 
         # 4a: Write an if statement that checks that the
         # county does not match any existing county in the county list.
-        if county_name not in county_list:
+        if county_name not in county_options:
 
             # 4b: Add the existing county to the list of counties.
-            county_list.append(county_name)
+            county_options.append(county_name)
 
             # 4c: Begin tracking the county's vote count.
             county_votes[county_name] = 0
@@ -89,37 +89,43 @@ with open(file_to_save, "w") as txt_file:
         f"-------------------------\n"
         f"Total Votes: {total_votes:,}\n"
         f"-------------------------\n\n"
-        f"County Votes:\n")
+        f"County Votes:\n"
+        f"-------------------------\n")
     print(election_results, end="")
 
     txt_file.write(election_results)
 
     # 6a: Write a for loop to get the county from the county dictionary.
-    for county_name in county_list:
+    for county_name in county_votes:
 
         # 6b: Retrieve the county vote count.
         votes = county_votes[county_name] 
         # 6c: Calculate the percentage of votes for the county.
-        percentage = votes/total_votes * 100
+        percentage = float(votes) / float(total_votes) * 100
 
          # 6d: Print the county results to the terminal.
-        print(percentage)
+        county_results = (
+            f"{county_name}: {percentage:.1f}% ({votes:,})\n")    
+        print(county_results)
 
          # 6e: Save the county votes to a text file.
-
+        txt_file.write(county_results)
          # 6f: Write an if statement to determine the winning county and get its vote count.
-        if votes > winning_countyvotes:
-            
-            winning_county = county_name
-            winning_countyvotes = votes
+        if (votes > winning_countycount) and (percentage > winning_countypercentage):
+                winning_countycounty = votes
+                winning_county = county_name
+                winning_countypercentage = percentage
 
         
 
     # 7: Print the county with the largest turnout to the terminal.
-
-
+    winning_county_summary = (
+        f"\n-------------------------\n"
+        f"Largest County Turnout: {winning_county}\n"
+        "-------------------------\n\n")
+    print(winning_county_summary)
     # 8: Save the county with the largest turnout to a text file.
-
+    txt_file.write(winning_county_summary)
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
@@ -128,7 +134,7 @@ with open(file_to_save, "w") as txt_file:
         votes = candidate_votes.get(candidate_name)
         vote_percentage = float(votes) / float(total_votes) * 100
         candidate_results = (
-            f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+            f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n\n")
 
         # Print each candidate's voter count and percentage to the
         # terminal.
